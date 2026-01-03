f2a697727506ba107fa798fa806fc3d2d2c2e127
waleska <waleska.s@malwee.com.br>
2026-01-03 16:23:06 -0300

    feat: add pokemon cubit, state and sorting logic

---
+++ lib/presentation/cubit/pokemon_cubit.dart
@@ -0,0 +1,73 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pokemon_pokedex/domain/entities/pokemon.dart';
+import 'package:pokemon_pokedex/domain/usecases/get_pokemons_usecase.dart';
+import 'package:pokemon_pokedex/presentation/cubit/pokemon_sort.dart';
+import 'package:pokemon_pokedex/presentation/cubit/pokemon_state.dart';
+
+class PokemonCubit extends Cubit<PokemonState> {
+  final GetPokemonsUsecase getPokemonsUsecase;
+
+  PokemonCubit(this.getPokemonsUsecase) : super(PokemonInitial());
+
+  List<Pokemon> _allPokemons = [];
+  String _query = '';
+  PokemonSort _sort = PokemonSort.alphabetical;
+
+  Future<void> fetchPokemons() async {
+    emit(PokemonLoading());
+
+    try {
+      _allPokemons = await getPokemonsUsecase.call();
+      _emitFilter();
+    } catch (e) {
+      emit(const PokemonError('Failed to load pokemons'));
+    }
+  }
+
+  void updateQuery(String value) {
+    _query = value;
+    _emitFilter();
+  }
+  
+  void updateSort(PokemonSort value) {
+    _sort = value;
+    _emitFilter();
+  }
+  
+  void _emitFilter() {
+    List<Pokemon> filter = List.from(_allPokemons);
+
+    if (_query.isNotEmpty) {
+      filter = filter.where((pokemon) {
+        final nameMatch =
+            pokemon.name.toLowerCase().contains(_query.toLowerCase());
+        final codeMatch = pokemon.id.toString().contains(_query);
+
+        return nameMatch || codeMatch;
+      }).toList();
+    }
+
+    switch (_sort) {
+      case PokemonSort.alphabetical:
+        filter.sort((a, b) => a.name.compareTo(b.name));
+        break;
+
+      case PokemonSort.codeAsc:
+        filter.sort((a, b) => a.id.compareTo(b.id));
+        break;
+
+      case PokemonSort.codeDesc:
+        filter.sort((a, b) => b.id.compareTo(a.id));
+        break;
+    }
+
+    emit(
+      PokemonLoaded(
+        pokemons: filter,
+        query: _query,
+        sort: _sort,
+      ),
+    );
+  }
+  
+}
---
+++ lib/presentation/cubit/pokemon_state.dart
@@ -0,0 +1,33 @@
+import 'package:equatable/equatable.dart';
+import 'package:pokemon_pokedex/domain/entities/pokemon.dart';
+import 'package:pokemon_pokedex/presentation/cubit/pokemon_sort.dart';
+
+abstract class PokemonState extends Equatable {
+  const PokemonState();
+  
+  @override
+  List<Object?> get props => [];
+}
+
+class PokemonInitial extends PokemonState {}
+
+class PokemonLoading extends PokemonState {}
+
+class PokemonLoaded extends PokemonState {
+  final List<Pokemon> pokemons;
+  final String query;
+  final PokemonSort sort;
+  
+  const PokemonLoaded({required this.pokemons, required this.query, required this.sort});
+
+  @override
+  List<Object?> get props => [pokemons];
+}
+
+class PokemonError extends PokemonState {
+  final String message;
+  const PokemonError(this.message);
+
+  @override
+  List<Object?> get props => [message];
+}
