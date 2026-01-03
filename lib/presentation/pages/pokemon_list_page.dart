811140d3ad13edf74ae5261622e5d32e15e2414c
waleska <waleska.s@malwee.com.br>
2026-01-03 16:24:50 -0300

    feat: add pokemon list page

---
+++ lib/presentation/pages/pokemon_list_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pokemon_pokedex/presentation/widgets/pokemon_list_filter.dart';
+import 'package:pokemon_pokedex/presentation/widgets/pokemon_list_header.dart';
+import '../cubit/pokemon_cubit.dart';
+import '../cubit/pokemon_state.dart';
+import '../widgets/pokemon_list_card.dart';
+
+class PokemonListPage extends StatefulWidget {
+  const PokemonListPage({super.key});
+
+  @override
+  State<PokemonListPage> createState() => _PokemonListPageState();
+}
+
+class _PokemonListPageState extends State<PokemonListPage> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<PokemonCubit>().fetchPokemons();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(child: PokemonListHeader()),
+
+          const SliverToBoxAdapter(
+            child: PokemonFilters(),
+          ),
+
+          BlocBuilder<PokemonCubit, PokemonState>(
+            builder: (context, state) {
+              if (state is PokemonLoading) {
+                return const SliverFillRemaining(hasScrollBody: false, child: Center(child: CircularProgressIndicator()));
+              }
+
+              if (state is PokemonLoaded) {
+                return SliverPadding(
+                  padding: const EdgeInsets.all(16),
+                  sliver: SliverGrid(
+                    delegate: SliverChildBuilderDelegate((context, index) {
+                      return PokemonCard(pokemon: state.pokemons[index]);
+                    }, childCount: state.pokemons.length),
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 16,
+                      crossAxisSpacing: 16,
+                      childAspectRatio: 0.75,
+                    ),
+                  ),
+                );
+              }
+
+              if (state is PokemonError) {
+                return SliverFillRemaining(hasScrollBody: false, child: Center(child: Text(state.message)));
+              }
+
+              return const SliverToBoxAdapter(child: SizedBox.shrink());
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
